fa0dfd63a78564c700911060b43726ecfc0f41ec
Renato Humberto <renatoqueiroz0233@gmail.com>
2025-06-04 12:00:47 +0000

    [feat]: dataflat

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TextInput, Button, FlatList } from 'react-native';
 import logo from "./assets/todolistlogo.png";
+import { useState } from 'react';
 
 export default function App() {
+  const [data, setData] = useState([
+
+    { id: "1", descricao: "Tarefa 1", data: "2025-01-01", realizado: true },
+    { id: "2", descricao: "Tarefa 2", data: "2025-01-02", realizado: false },
+    { id: "3", descricao: "Tarefa 3", data: "2025-01-03", realizado: true },
+    { id: "4", descricao: "Tarefa 4", data: "2025-01-04", realizado: false },
+    { id: "5", descricao: "Tarefa 5", data: "2025-01-05", realizado: false },
+  ]);
+  
+  const item = () => {
+    return (
+      <View>
+        <Text>{item.realizado}</Text>
+        <View>
+          <Text>{item.descricao}</Text>
+          <Text>{item.data}</Text>
+        </View>
+      </View>
+    )
+  };
+
   return (
     <View style={styles.container}>
       <View>
-        <Image source={logo} style={styles.logo}/>
+        <Image source={logo} style={styles.logo} />
         <Text style={styles.textLogo}>TODO List</Text>
       </View>
       <View style={styles.viewTask}>
-        <TextInput style={styles.inputTask} placeholder='Digite a Tarefa'/>
-        <Button title="Adicionar"/>
+        <TextInput style={styles.inputTask} placeholder='Digite a Tarefa' />
+        <Button title="Adicionar" />
       </View>
-      <FlatList />
+      <FlatList style={styles.taskList}
+        data={data}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => <Text>{item.descricao}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -31,24 +57,24 @@ const styles = StyleSheet.create({
     height: 300,
     width: 300,
   },
-  viewTask:{
+  viewTask: {
     flexDirection: "row",
     marginTop: 10,
   },
-  inputTask:{
+  inputTask: {
     flex: 1,
     borderColor: "gray",
     borderWidth: 1,
     paddingLeft: 10,
     marginRight: 10,
   },
-  viewLogo:{
+  viewLogo: {
     alignItems: "center"
   },
-  textLogo:{
+  textLogo: {
     justifyContent: "center",
   },
   taskList: {
-    flex:1,
+    flex: 1,
   }
 });
